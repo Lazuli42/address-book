709502daa942c1e9083c6eb97de1b1bb40e6cd5c
Zachary Baum <zacharybaum42@gmail.com>
2016-09-09 10:26:05 -0400

    initialize repository

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Contact.php";
+
+    session_start();
+    if (empty($_SESSION['contacts_list'])) {
+        $_SESSION['contacts_list'] = array();
+    }
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render( 'home.html.twig', array ("contacts" => Contact::getAll()) );
+    });
+
+    $app->post("/new_contact", function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render( 'home.html.twig', array ("contacts" => Contact::getAll()) );
+    });
+
+    return $app;
+ ?>
---
+++ src/Contact.php
@@ -0,0 +1,20 @@
+<?php
+    class Contact
+    {
+        private $name;
+        private $phone;
+        private $address;
+
+        function __construct($name, $phone, $address)
+        {
+            $this->name = $name;
+            $this->phone = $phone;
+            $this->address = $address;
+        }
+
+        function setName($new_name)
+        {
+            
+        }
+    }
+?>
